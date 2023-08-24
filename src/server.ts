e08a844ecf2edd5451fe58216fadfbe82646048e
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-24 20:09:39 +0600

    Add morgan for logging and cors security

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,16 @@ import userRouter from "./routes/user";
 import notesRouter from "./routes/notes";
 import bodyParser from "body-parser";
 import { protectedRoute } from "./middleware/auth";
+import morgan from "morgan"
+import cors from "cors"
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 bodyParser.urlencoded({ extended: true });
+
+app.use(morgan("combined"))
+app.use(cors())
 
 app.get("/", protectedRoute, (req, res) => {
   res.send("hello");
