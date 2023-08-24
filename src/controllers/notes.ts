db5f3b34d09993a053853facea59e7c49e3217fc
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-24 16:34:56 +0600

    Rework controllers

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -1,76 +1,75 @@
-import { Request, Response, NextFunction } from "express"
-import Notes from "../models/notes"
-import { NotesTypes } from "../types"
-import { UserTypes } from "../types"
+import { Request, Response } from "express";
+import Notes from "../models/notes";
+import asyncHandler from "../middleware/asyncHandler"
 
-export const getAllNotes = async (req: Request, res: Response, next: NextFunction) => {
-  const notes = await Notes.find({ user: req.user!.id })
-  res.status(200)
-  res.json({ notes })
-}
+export const getAllNotes = asyncHandler(async (req: Request, res: Response) => {
+  const notes = await Notes.find({ user: req.user!.id });
+  if (!notes) {
+    res.status(200);
+    res.json({ message: "You have no notes" });
+  }
+  res.status(200);
+  res.json({ notes });
+});
 
-export const createOneNote = async (req: Request, res: Response, next: NextFunction) => {
+export const createOneNote = asyncHandler(async (req: Request, res: Response) => {
   const note = await Notes.create({
     user: req.user!.id,
     title: req.body.title,
-    body: req.body.body
-  })
-  res.status(200)
-  res.json({ message: "New note saved", note })
-}
+    body: req.body.body,
+  });
+  res.status(200);
+  res.json({ message: "New note saved", note });
+});
 
-export const getOneNote = async (req: Request, res: Response, next: NextFunction) => {
-  const note = await Notes.findById(req.params.id)
-  if(!note) {
-    res.status(400)
-    res.json({message: "Note not found"})
-    return
-  }
-  if (note!.user.toString() === req.user!.id) {
-    res.status(200)
-    res.json({ note })
-    return
-  }
-  res.status(403)
-  res.json({ message: "You don't have permission to view this note" })
-}
-
-export const updateOneNote = async (req: Request, res: Response, next: NextFunction) => {
-  const note = await Notes.findById(req.params.id)
-  if (!note) {
-    res.status(400)
-    res.send("note not found")
-    return
-  }
-  if (note!.user.toString() === req.user!.id) {
-    note.title = req.body.title
-    note.body = req.body.body
-
-    const updatedNote = await note.save()
-
-    console.log(updatedNote)
-    res.status(200)
-    res.json({ updatedNote })
-    return
-  }
-  res.json({ message: "You don't have permission" })
-}
-
-export const deleteOneNote = async (req: Request, res: Response, next: NextFunction) => {
+export const getOneNote = asyncHandler(async (req: Request, res: Response) => {
   const note = await Notes.findById(req.params.id);
   if (!note) {
-    res.status(400)
-    res.send("Note not found")
-    return
+    res.status(400);
+    res.json({ message: "Note not found" });
+    return;
+  }
+  if (note.user.toString() === req.user!.id) {
+    res.status(200);
+    res.json({ note });
+    return;
+  }
+  res.status(403);
+  res.json({ message: "You don't have permission to view this note" });
+});
+
+export const updateOneNote = asyncHandler(async (req: Request, res: Response) => {
+  const note = await Notes.findById(req.params.id);
+  if (!note) {
+    res.status(400);
+    res.send("Note not found");
+    return;
+  }
+  if (note!.user.toString() === req.user!.id) {
+    note.title = req.body.title;
+    note.body = req.body.body;
+
+    const updatedNote = await note.save();
+
+    res.status(200);
+    res.json({ updatedNote });
+    return;
+  }
+  res.json({ message: "You don't have permission" });
+});
+
+export const deleteOneNote = asyncHandler(async (req: Request, res: Response) => {
+  const note = await Notes.findById(req.params.id);
+  if (!note) {
+    res.status(400);
+    res.send("Note not found");
+    return;
   }
 
   if (note!.user.toString() === req.user!.id) {
-    console.log("run")
-    await Notes.deleteOne({_id: note._id})
-    console.log("run")
+    await Notes.deleteOne({ _id: note._id });
     res.json({ message: "Note deleted successfully!" });
-    return
+    return;
   }
-  res.json({message: "You don't have permission"})
-
-}
+  res.json({ message: "You don't have permission" });
+});
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,34 +1,34 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import asyncHandler from "../middleware/asyncHandler";
-import {createJWT, hashPassword} from "../middleware/auth"
+import { createJWT, hashPassword } from "../middleware/auth";
 import User from "../models/user";
 
-export const SignUp = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const userExists = await User.findOne({email: req.body.email})
-  if(userExists) {
-    res.status(500)
-    res.json({"message": "User already exits!"})
-    next()
+export const SignUp = asyncHandler(async (req: Request, res: Response) => {
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists) {
+    res.status(500);
+    res.json({ message: "User already exits!" });
+    return;
   }
   const user = await User.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     password: await hashPassword(req.body.password),
-  })
+  });
 
-  res.status(200)
-  res.json({message: "User created", user})
-})
+  res.status(200);
+  res.json({ message: "User created", user });
+});
 
-export const LogIn = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const user = await User.findOne({email: req.body.email})
-  if(!user) {
-    res.status(400)
-    res.json({message: "User doesn't exits!"})
-    next()
+export const LogIn = asyncHandler(async (req: Request, res: Response) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    res.status(400);
+    res.json({ message: "User doesn't exits!" });
+    return;
   }
-  const token = createJWT(user)
-  res.status(200)
-  res.json({user, token})
-})
+  const token = createJWT(user);
+  res.status(200);
+  res.json({ user, token });
+});
