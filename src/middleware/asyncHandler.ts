702ac37173f39fc4612ec562cd5c11a14a9c4114
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-11-15 18:24:32 +0600

    Rework asyncHandler to named function

--- src/middleware/asyncHandler.ts
+++ src/middleware/asyncHandler.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
-const asyncHandler =
-  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
+type fn = (req: Request, res: Response, next: NextFunction) => void
+
+function asyncHandler(fn: fn) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+}
 
 export default asyncHandler;
